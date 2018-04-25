864ad2270a6f3a1604663107289b8ae54656b82d
liutao <liutao@yugusoft.com>
2018-04-25 11:41:31 +0800

    【ScreenCapture】截屏进程保持运行状态，通过socket通信

--- ScreenCapture/main.cpp
+++ ScreenCapture/main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QTcpServer>
+#include <QTcpSocket>
 
 #ifdef Q_OS_WIN
 #include "ScreenshotWidget.h"
@@ -6,17 +8,72 @@
 #include "ScreenshotWidgetmac.h"
 #endif
 
+#define PORT 50263
+
+QTcpServer *server = NULL;
+QList<QTcpSocket *> socketList;
+
+ScreenshotWidget *shotWidget = NULL;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setQuitOnLastWindowClosed(false);
 
-    ScreenshotWidget w;
-    w.connect(&w, &ScreenshotWidget::sigShotScreenFinished, &a, &QApplication::quit);
+    QObject::connect(&a, &QApplication::aboutToQuit, [](){
+        if(shotWidget)
+        {
+            shotWidget->deleteLater();
+            shotWidget = NULL;
+        }
 
+        if(server)
+        {
+            server->close();
+            server->deleteLater();
+            server = NULL;
+        }
+
+        while(socketList.count())
+        {
+            QTcpSocket *socket = socketList.takeFirst();
+            socket->disconnectFromHost();
+            socket->close();
+            socket->deleteLater();
+        }
+    });
+
+    server = new QTcpServer();
+    QObject::connect(server, &QTcpServer::newConnection, [](){
+        QTcpSocket * socket = server->nextPendingConnection();
+        QObject::connect(socket, &QTcpSocket::readyRead, [socket](){
+            QByteArray buffer = socket->readAll();
+            if(buffer == "start")
+            {
+                if(shotWidget)
+                {
+                    shotWidget->close();
+                    shotWidget->deleteLater();
+                    shotWidget = NULL;
+                }
+
+                shotWidget = new ScreenshotWidget();
 #ifdef Q_OS_WIN
-    w.getReadyToShot();
-    w.show();
+                shotWidget->getReadyToShot();
+                shotWidget->show();
 #endif
+                shotWidget->raise();
+                shotWidget->activateWindow();
+            }
+        });
+        QObject::connect(socket, &QTcpSocket::disconnected, [socket](){
+            socket->close();
+            socket->deleteLater();
+            socketList.removeAll(socket);
+        });
+        socketList.append(socket);
+    });
+    server->listen(QHostAddress::Any, PORT);
 
     return a.exec();
 }
